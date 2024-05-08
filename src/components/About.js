f5360d0daeb4cf1398a6088d4cf01cbb915a09e8
t0nylombardi <alombardi.331@gmail.com>
2024-05-08 14:38:35 -0400

    add props to children

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react';
+import Links from './Links';
 
-function About() {
+
+function About(props) {
   return (
-    <div id="about">
+    <div id='about'>
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio ? <p>{props.bio}</p> : ''}
+      <img src='https://i.imgur.com/mV8PQxj.gif' alt='I made this' />
+      <Links linkedin={props.links.linkedin} github={props.links.github}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Links = (props) => {
+
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.linkedin}>{props.linkedin}</a>
+      <a href={props.github}>{props.github}</a>
+    </div>
+  )
+}
+
+export default Links
